a725891664c76064f6dbdfa88963d6c564aee8ed
AurelienBesnier <aurelienbesnier.pro@gmail.com>
2026-05-20 17:44:36 +0200

    Computing bounding spher

--- scripts/crop_sphere.gd
+++ scripts/crop_sphere.gd
@@ -18,10 +18,31 @@ extends MeshInstance3D
 const SCALE_INCREMENT = 0.05
 const MOVE_INCREMENT = 0.05
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+var sphere_center: Vector3
+var sphere_radius: float
 
+func _ready() -> void:
+	pass
+
+func calculate_bounding_sphere() -> void:
+	var local_aabb = get_aabb()
+	var local_center = local_aabb.position + (local_aabb.size / 2.0)
+	sphere_center = global_transform * local_center
+	
+	var local_radius = local_center.distance_to(local_aabb.position)
+	
+	var s = global_transform.basis.get_scale()
+	var max_scale = max(s.x, max(s.y, s.z))
+	
+	sphere_radius = local_radius * max_scale
+	
+	
+func is_point_inside(target_point: Vector3) -> bool:
+	var dist_squared: float = sphere_center.distance_squared_to(target_point)
+	var radius_squared: float = sphere_radius * sphere_radius
+	
+	return dist_squared <= radius_squared
+	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
@@ -30,12 +51,14 @@ func _process(delta: float) -> void:
 	
 func process_actions(_delta: float):
 	if Input.is_action_pressed(scale_up):
-		var size = self.mesh.size
-		self.mesh.size = Vector3(size.x+SCALE_INCREMENT,size.y+SCALE_INCREMENT,size.z+SCALE_INCREMENT)
+		var size = self.mesh.radius
+		self.mesh.radius = size + SCALE_INCREMENT
+		self.mesh.height = size * 2 + SCALE_INCREMENT
 	if Input.is_action_pressed(scale_down):
-		var size = self.mesh.size
-		if size.x >= 0.1:
-			self.mesh.size = Vector3(size.x-SCALE_INCREMENT,size.y-SCALE_INCREMENT,size.z-SCALE_INCREMENT)
+		var size = self.mesh.radius
+		if size >= 0.1:
+			self.mesh.radius = size - SCALE_INCREMENT
+			self.mesh.height = size * 2 - SCALE_INCREMENT
 
 	# Movement
 	if Input.is_action_pressed(move_x):
@@ -59,15 +82,14 @@ func process_actions(_delta: float):
 		reset_selection()
 		
 func select_points() -> void:
-	var aabb = self.get_aabb()
-	
+	calculate_bounding_sphere()
 	var number_selected = 0
 	
 	for i in range(splat_mesh_instance.multimesh.instance_count):
 		var global_pos = to_local(splat_mesh_instance.multimesh.get_instance_transform(i).origin)
 		var color = splat_mesh_instance.multimesh.get_instance_color(i)
 
-		if not aabb.has_point(global_pos):
+		if not self.is_point_inside(global_pos):
 			color.a8 = 3
 			splat_mesh_instance.multimesh.set_instance_color(i, color)
 		else:
@@ -88,5 +110,3 @@ func reset_selection() -> void:
 	for i in range(splat_mesh_instance.multimesh.instance_count):
 		var instance_col = splat_mesh_instance.multimesh.get_instance_color(i)
 		instance_col.a8 = 255
-		splat_mesh_instance.multimesh.set_instance_color(i, instance_col)
-		
