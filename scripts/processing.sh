e5e2a81f2cf190c3f9c9b71ec998dc003e3dd26b
AurelienBesnier <aurelienbesnier.pro@gmail.com>
2026-05-25 18:20:56 +0200

    try catching

--- scripts/processing.sh
+++ scripts/processing.sh
@@ -1,11 +1,13 @@
 echo "this is in the bash script: $HOME"
 
-# initialize mamba for the script
-eval "$(mamba shell hook --shell bash)"
-mamba activate splat
+{
+    conda activate splat
+} || {
 
-# TODO: make a conda fallback if needed
-
+    # initialize mamba for the script
+    eval "$(mamba shell hook --shell bash)"
+    mamba activate splat
+}
 
 
 filtering -s $1 -c $2
