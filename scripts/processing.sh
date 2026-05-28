38d4c71baf678ceb602c8adcc1795c638ddc535a
AurelienBesnier <aurelienbesnier.pro@gmail.com>
2026-05-28 11:16:19 +0200

    conda compatible run command

--- scripts/processing.sh
+++ scripts/processing.sh
@@ -1,11 +1,9 @@
 {
-	conda activate splat
+	conda run -n splat filtering -s $1 -c $2
 } || {
 
 	# initialize mamba for the script
 	eval "$(mamba shell hook --shell bash)"
 	mamba activate splat
+	filtering -s $1 -c $2
 }
-
-
-filtering -s $1 -c $2
