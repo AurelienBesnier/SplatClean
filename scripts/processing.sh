38518318420ba818f382d4043c5fc51f425aca44
AurelienBesnier <aurelienbesnier.pro@gmail.com>
2026-05-28 12:10:58 +0200

    Using analysis workflow

--- scripts/processing.sh
+++ scripts/processing.sh
@@ -1,9 +1,9 @@
 {
-	conda run -n splat filtering -s $1 -c $2
+	conda run -n splat analysis -s $1 -c $2
 } || {
 
 	# initialize mamba for the script
 	eval "$(mamba shell hook --shell bash)"
 	mamba activate splat
-	filtering -s $1 -c $2
+	analysis -s $1 -c $2
 }
