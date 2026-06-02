4a6d63a114a5e8c83f4141f0dabcae1a38d06c13
AurelienBesnier <aurelienbesnier.pro@gmail.com>
2026-06-02 20:11:48 +0200

    Adding background loading of objects in mesh veiw

--- scripts/file_io.gd
+++ scripts/file_io.gd
@@ -278,18 +278,23 @@ func _on_process_finished(exit_code: int, output: Array):
 	if output.size() > 0:
 		print("Output:\n", output[0])
 	if FileAccess.file_exists("Archicrop.obj"): # Display result in seperate window
-		var scene_resource = load("res://scenes/MeshView.tscn")
-		var scene_instance = scene_resource.instantiate()
+		var load_thread = Thread.new()
+		var thread_callable = _load_window_background.bind()
+		load_thread.start(thread_callable)
 		
-		var new_window = Window.new()
-		new_window.title = "Mesh Viewer"
-		new_window.size = Vector2i(800, 600)
-		new_window.transient = false 
-		new_window.world_3d = World3D.new()
-		new_window.add_child(scene_instance)
-		new_window.close_requested.connect(func(): new_window.queue_free())
-		get_tree().root.add_child(new_window)
-		new_window.position = DisplayServer.window_get_position() + Vector2i(50, 50)
+func _load_window_background():
+	var scene_resource = load("res://scenes/MeshView.tscn")
+	var scene_instance = scene_resource.instantiate()
+	
+	var new_window = Window.new()
+	new_window.title = "Mesh Viewer"
+	new_window.size = Vector2i(800, 600)
+	new_window.transient = false 
+	new_window.world_3d = World3D.new()
+	new_window.add_child(scene_instance)
+	new_window.close_requested.connect(func(): new_window.queue_free())
+	get_tree().root.call_deferred("add_child", new_window)
+	new_window.position = DisplayServer.window_get_position() + Vector2i(50, 50)
 #endregion
 
 func _on_h_slider_value_changed(value) -> void:
